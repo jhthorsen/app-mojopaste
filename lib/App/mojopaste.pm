d176f8be2f2c6ba772465a4b2c1ba91c0ef5a039
Jan Henning Thorsen <jhthorsen@cpan.org>
2013-09-05 16:48:08 +0200

    Released version 0.02
           * Fix repository path

--- lib/App/mojopaste.pm
+++ lib/App/mojopaste.pm
@@ -6,7 +6,7 @@ App::mojopaste - Pastebin application
 
 =head1 VERSION
 
-0.01
+0.02
 
 =head1 DESCRIPTION
 
@@ -72,7 +72,7 @@ Check out L<Mojo::Server::Hypnotoad> for more hypnotoad options.
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 =head1 AUTHOR
 
