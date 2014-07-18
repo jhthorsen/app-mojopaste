275ab5fdf48fbb01d836d777e702a98470e93734
Jan Henning Thorsen <jhthorsen@cpan.org>
2014-07-18 02:13:45 +0200

    Released version 0.06
           * Change bugracker

--- lib/App/mojopaste.pm
+++ lib/App/mojopaste.pm
@@ -6,7 +6,7 @@ App::mojopaste - Pastebin application
 
 =head1 VERSION
 
-0.0501
+0.06
 
 =head1 DESCRIPTION
 
@@ -76,7 +76,7 @@ Check out L<Mojo::Server::Hypnotoad> for more hypnotoad options.
 
 =cut
 
-our $VERSION = '0.0501';
+our $VERSION = '0.06';
 
 =head1 AUTHOR
 
