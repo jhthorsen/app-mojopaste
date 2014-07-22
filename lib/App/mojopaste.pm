a70b0ece35aa235579d3845471e6e4ea1d3ae890
Jan Henning Thorsen <jhthorsen@cpan.org>
2014-07-22 17:00:22 +0200

    Released version 0.08
           * Compatible with Mojolicious 5.xx

--- lib/App/mojopaste.pm
+++ lib/App/mojopaste.pm
@@ -6,7 +6,7 @@ App::mojopaste - Pastebin application
 
 =head1 VERSION
 
-0.07
+0.08
 
 =head1 DESCRIPTION
 
@@ -76,7 +76,7 @@ Check out L<Mojo::Server::Hypnotoad> for more hypnotoad options.
 
 =cut
 
-our $VERSION = '0.07';
+our $VERSION = '0.08';
 
 =head1 AUTHOR
 
