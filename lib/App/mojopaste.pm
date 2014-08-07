12f640d304a4505886f43f425d693403d541c1c3
Jan Henning Thorsen <jhthorsen@cpan.org>
2014-08-07 17:51:28 +0200

    Released version 0.10
           * Fix button border styling, #1

--- lib/App/mojopaste.pm
+++ lib/App/mojopaste.pm
@@ -6,7 +6,7 @@ App::mojopaste - Pastebin application
 
 =head1 VERSION
 
-0.09
+0.10
 
 =head1 DESCRIPTION
 
@@ -76,7 +76,7 @@ Check out L<Mojo::Server::Hypnotoad> for more hypnotoad options.
 
 =cut
 
-our $VERSION = '0.09';
+our $VERSION = '0.10';
 
 =head1 AUTHOR
 
