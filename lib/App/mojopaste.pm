862ae7cd509cf9465e62aaa72422785737446e18
Jan Henning Thorsen <jhthorsen@cpan.org>
2016-01-13 09:25:53 +0100

    Released version 0.16
     - Recommends JSON::Syck
     - Recommends Text::CSV
     - No need to encode/decode UTF-8 anymore

--- lib/App/mojopaste.pm
+++ lib/App/mojopaste.pm
@@ -6,7 +6,7 @@ App::mojopaste - Pastebin application
 
 =head1 VERSION
 
-0.15
+0.16
 
 =head1 DESCRIPTION
 
@@ -153,7 +153,7 @@ Check out L<Mojo::Server::Hypnotoad> for more hypnotoad options.
 
 =cut
 
-our $VERSION = '0.15';
+our $VERSION = '0.16';
 
 =head1 AUTHOR
 
