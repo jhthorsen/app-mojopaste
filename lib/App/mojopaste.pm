48f27d8178ef713539fd955ac1d26f0122b57497
Jan Henning Thorsen <jhthorsen@cpan.org>
2016-06-29 18:08:34 +0200

    Released version 0.20
     - Fix X-Plain-Text-URL must be an absolute URL
     - Add line numbers to pretty printed code
     - Change "Paste" button to "Save"

--- lib/App/mojopaste.pm
+++ lib/App/mojopaste.pm
@@ -1,6 +1,6 @@
 package App::mojopaste;
 
-our $VERSION = '0.19';
+our $VERSION = '0.20';
 
 1;
 
@@ -12,7 +12,7 @@ App::mojopaste - Pastebin application
 
 =head1 VERSION
 
-0.19
+0.20
 
 =head1 DESCRIPTION
 
