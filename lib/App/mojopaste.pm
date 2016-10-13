8f210d902e3cceac80d1cb514d22afbdf562eca3
Jan Henning Thorsen <jhthorsen@cpan.org>
2016-10-13 10:01:56 +0200

    Released version 0.24
     - Writing/reading files are more robust

--- lib/App/mojopaste.pm
+++ lib/App/mojopaste.pm
@@ -1,6 +1,6 @@
 package App::mojopaste;
 
-our $VERSION = '0.23';
+our $VERSION = '0.24';
 
 1;
 
@@ -12,7 +12,7 @@ App::mojopaste - Pastebin application
 
 =head1 VERSION
 
-0.23
+0.24
 
 =head1 DESCRIPTION
 
