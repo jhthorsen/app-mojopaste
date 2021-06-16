679f4d052022cb37f68e4591d9dc32a2cd4844af
Jan Henning Thorsen <jhthorsen@cpan.org>
2021-06-17 07:56:54 +0900

    Released version 1.05
     - Compatible with Mojolicious 9.11
       Contributor: Dan Book

--- lib/App/mojopaste.pm
+++ lib/App/mojopaste.pm
@@ -1,6 +1,6 @@
 package App::mojopaste;
 
-our $VERSION = '1.04';
+our $VERSION = '1.05';
 
 1;
 
@@ -12,7 +12,7 @@ App::mojopaste - Pastebin application
 
 =head1 VERSION
 
-1.04
+1.05
 
 =head1 DESCRIPTION
 
